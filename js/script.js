9e7ae861ab7b7df357826f8d0eeab5dba9ccb73a
Marla Pino Puentes <marlapinopuentes@gmail.com>
2024-09-28 12:18:05 -0300

    Add offcanvas and dropdown to show more info

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 let moviesData = [];
-const moviesContainer = document.getElementById("lista");
 
 getJSONData(MOVIES_LIST_URL).then(function (resultObj) {
     if (resultObj.status === "ok") {
@@ -8,17 +7,29 @@ getJSONData(MOVIES_LIST_URL).then(function (resultObj) {
 });
 
 // Mostrar las peliculas
+const moviesContainer = document.getElementById("lista");
+
 function showMoviesData(moviesArray) {
     moviesContainer.innerHTML = "";
-    for (const item of moviesArray) {
-        moviesContainer.innerHTML += `<li class="row border border-light rounded pt-3 pb-1 mb-1">
-                 <div class="col-9">
-                     <h3>${item.title}</h3>
-                     <p><em>${item.tagline}</em></p>
-                 </div>
-                 <div class="col-3 text-center">
+    for (let item of moviesArray) {
+        let list = document.createElement('li');
+        list.className = "row border border-light rounded p-2 mb-1";
+        list.setAttribute("data-bs-toggle", "offcanvas");
+        list.setAttribute("data-bs-target", "#offcanvasTop");
+        list.setAttribute("aria-controls", "offcanvasTop");
+        list.style.cursor = "pointer";
+        list.innerHTML = `
+                <div class="col-9">
+                     <h4>${item.title}</h4>
+                     <p class="text-secondary"><em>${item.tagline}</em></p>
+                </div>
+                <div class="col-3 text-center my-auto">
                     ${drawStars(item.vote_average)}
-                 </div>`
+                </div>`
+        list.onclick = () => {
+            showDetails(item);
+        } 
+        moviesContainer.appendChild(list);
     }
 }
 
@@ -70,11 +81,35 @@ function drawStars(rating) {
     for (let i = 1; i <= 10; i++) {
         if (i <= rating) {
             ratingHTML += '<span class="fa fa-star checked"></span>';
-        } else if ((i - 0.5) <= rating /*&& i > rating*/) {
+        } else if ((i - 0.5) <= rating) {
             ratingHTML += '<span class="fa fa-star half-checked"></span>';
         } else {
             ratingHTML += '<span class="fa fa-star not-checked"></span>';
         }
     }
     return ratingHTML;
+}
+
+// Funcion para mostrar información adicional de la pelicula
+function showDetails(movie){
+    document.getElementById('offcanvasTopLabel').textContent = movie.title;
+    document.getElementById('offcanvas-body-main').textContent = movie.overview;
+    let genresList = document.getElementById('offcanvas-body-footer');
+    let genresText = movie.genres.map(genre => genre.name).join(' - '); 
+    genresList.textContent = genresText; 
+    let offcanvasFooter = document.querySelector('#offcanvas-body-footer');
+    let dropdownMenu = `
+    <div class="dropdown-center text-end">
+    <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+      More
+    </button>
+    <ul class="dropdown-menu">
+      <li class="dropdown-item">Year: ${new Date(movie.release_date).getFullYear()}</li>
+      <li class="dropdown-item">Runtime: ${movie.runtime} mins</li>
+      <li class="dropdown-item">Budget: $${movie.budget.toLocaleString()}</li>
+      <li class="dropdown-item">Revenue: $${movie.revenue.toLocaleString()}</li>
+    </ul>
+  </div>
+     `;
+    offcanvasFooter.innerHTML += dropdownMenu;
 }
